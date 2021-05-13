c4b8a7a533638c0586e665722f99e0b5b0246fe1
KirinZZF <930216299@qq.com>
2021-05-13 03:16:32 +0100

    1. add chatlist screen to project 2. userchatlist reducer action store 3. contact seller, if have not a chat before, create and to chatlist 4. when send a message, store to db first then notify the receiver

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,15 @@ import schedule from 'node-schedule';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import handleExpiredOrders from './middleware/expireMiddleware.js';
 import connectDB from './config/db.js';
-
+import {createChat, updateChat} from './controllers/userChatController.js'
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
-
-dotenv.config({ path: '../.env' });
+import { createServer } from "http";
+import { Server } from "socket.io";
+import userChatRoutes from './routes/userChatRoutes.js'
+dotenv.config();
 console.log(process.env.MONGO_URI);
 connectDB();
 
@@ -37,7 +39,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
-
+app.use('/api/chatlist', userChatRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
@@ -60,13 +62,57 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const httpServer = createServer(app);
+const io = new Server(httpServer,{
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+  }
+});
 
-app.listen(
+const PORT = process.env.PORT || 5000
+
+let socketPool =[]
+io.on("connection", (socket)=>{
+  console.log(socket.handshake.headers["my-custom-header"]+" connected")
+  socketPool.push({
+    socketId:socket.id,
+    socketUserId:socket.handshake.headers["my-custom-header"],
+  })
+  socket.on("private-message",(chatId, message)=>{
+    //socket.emit("private-message",chatId,message)
+    console.log(socketPool.length)
+    for(var sock of socketPool){
+      console.log(sock.socketId+"   "+sock.socketUserId)
+      console.log(JSON.stringify(sock))
+      if(sock.socketUserId==message.destUser._id){
+        console.log("要在chatId:"+chatId+"中发"+JSON.stringify(message.msgContent))
+        //socket.emit("private-message",chatId,message)
+        updateChat(chatId,message)
+        // if(result=='failed'){
+        //   console.log("failed to save chat"+chatId)
+        //   break
+        // }
+        socket.to(sock.socketId).emit("private-message",chatId,message)
+        break
+      }
+    }
+  })
+
+  socket.on("createNewChat",(newchat)=>{
+    console.log("创建新的聊天")
+    console.log(JSON.stringify(newchat))
+    createChat(newchat)
+  })
+})
+
+
+httpServer.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-);
+)
 
 scanExpiredOreders();
